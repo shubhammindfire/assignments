e384d0d48435520fb962b55851d773cddcd81797
shubhammindfire <shubhamk@mindfiresolutions.com>
2021-02-12 22:34:42 +0530

    Fixed bugs in AJAX assignment

--- 11 Feb/AJAX assignment/assets/js/script.js
+++ 11 Feb/AJAX assignment/assets/js/script.js
@@ -3,79 +3,62 @@
 // This function shows all the API data in tabular format
 function loadData() {
     var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+    xhr.send();
     xhr.onload = function () {
         if (this.status == 200) {
             // parsing json data received as response from the API
             let json = JSON.parse(this.responseText);
             let html = `
-                <table>
+                <table class="table table-hover table-bordered">
                     <thead>
                         <th>userId</th>
                         <th>id</th>
                         <th>title</th>
-                        <th>body</th>
                     </thead>
                 `;
 
             // looping through all the json objects and adding it to the table
             for (var i in json) {
-                // replacing \n escape sequence with a blank space
-                json[i].body = (json[i].body).replace("\n", " ");
-
-                console.log(json[i].body);
-
                 html += `
-                    <tr onclick="getIndividualData(${json[i].userId},${json[i].id},'${json[i].title}')">
+                    <tr onclick="getIndividualData(${json[i].id})" data-toggle="modal" data-target="#myModal">
                         <td>${json[i].userId}</td>
                         <td>${json[i].id}</td>
                         <td>${json[i].title}</td>
-                        <td>${json[i].body}</td>
                     </tr>
                 `;
-
-
-                /*html += `
-                    <tr class='row'>
-                        <td>${json[i].userId}</td>
-                        <td>${json[i].id}</td>
-                        <td>${json[i].title}</td>
-                        <td>${json[i].body}</td>
-                    </tr>
-                `;*/
             }
 
             html += "</table>";
             // update the complete table in the HTML
             document.getElementById("table").innerHTML = html;
-
-            //var row = document.getElementsByClassName("row");
-            //row[0].addEventListener("click",getIndividualData(11,23,'title','body'));
-
         }
     };
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+}
+
+// This function shows the individual data in a modal
+function getIndividualData(id){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/"+id, true);
     xhr.send();
+    xhr.onload = function () {
+        if (this.status == 200) {
+            // parsing json data received as response from the API
+            let json = JSON.parse(this.responseText);
+            
+            document.getElementById("json-body").innerHTML = json.body;
+        }
+    };
 }
 
-// This function shows the individual data in tabular format
-function getIndividualData(userId,id,title,body){
-    let table = `
-        <table>
-            <thead>
-                <th>userId</th>
-                <th>id</th>
-                <th>title</th>
-                <th>body</th>
-            </thead>
-            <tr>
-                <td>${userId}</td>
-                <td>${id}</td>
-                <td>${title}</td>
-                <td>${body}</td>
-            </tr>
-        </table>
-    `;
+// I wrote JS code similar to the below jQuery but this isn't working
+/*
+document.getElementById("myModal").addEventListener('hide.bs.modal',function(){
+    console.log("hide");
+});
+*/
 
-    // update table in the HTML
-    document.getElementById('data').innerHTML = table;
-}
+// Changing modal body to "Please wait" everytime modal is closed
+$("#myModal").on('hide.bs.modal', function(){
+    document.getElementById("json-body").innerHTML = "Please wait...";
+});
