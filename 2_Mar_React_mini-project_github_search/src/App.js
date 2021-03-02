9651607327ba3dc1edca14bd51f641d7bc0e461e
shubhammindfire <shubhamk@mindfiresolutions.com>
2021-03-02 11:33:48 +0530

    removing node_modules

---
+++ 2_Mar_React_mini-project_github_search/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GithubSearch from './components/GithubSearch.js';
+
+function App() {
+  return (
+    <div className="App">
+      <GithubSearch />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_Mar_React_mini-project_github_search/src/components/GithubSearch.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "./../components/Search.js";
+
+function GithubSearch() {
+    return (
+        <div>
+            <h2>Github Search</h2>
+            <p>Search users across Github</p>
+            <Search />
+        </div>
+    );
+}
+
+export default GithubSearch;
