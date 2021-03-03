56d54c9cda17ea12d89ba72f4fdbf91a54f3b172
shubhammindfire <shubhamk@mindfiresolutions.com>
2021-03-03 14:15:50 +0530

    Bug Fix: using useState hook and avoiding direct access of DOM

--- 2_Mar_React_mini-project_github_search/src/components/Search.js
+++ 2_Mar_React_mini-project_github_search/src/components/Search.js
@@ -1,115 +1,153 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+// TODO: USE useEffect() hook as suggested by Sobhan
+// TODO: USE ReactStrap
 function Search() {
-  // https://api.github.com/users/
+    // https://api.github.com/users/
 
-  const [user, changeUser] = useState("");
-  const [login, updateLogin] = useState("");
+    const [user, changeUser] = useState("");
+    const [login, updateLogin] = useState("");
+    const [userInfo, updateUserInfo] = useState({});
+    const [errorText, updateErrorText] = useState("");
 
-  function addHTML(res) {
-    // bio followers following location
-    console.log(`res = ${res} ${res.data.login} ${res.data.name}`);
-    let html = "";
-    if (res.data.avatar_url)
-      html += `<div style="margin-top:20px;"><img style="border-radius:50%;width:150px;height:150px;" src=${res.data.avatar_url} /></div>`;
+    function addHTML(res) {
+        console.log(
+            `res = ${res} ${res.data.login} ${res.data.name} ${res.data.avatar_url}`
+        );
 
-    html += `<div style="margin-top:20px;">Username: <a href=${res.data.html_url}>${res.data.login}</a></div>`;
+        // using a separate object {obj} because the spread operator wasn't working
+        // well with the {updateUserInfo} callback!!
+        // at the end of this function I update {updateUserInfo} with {obj}
+        let obj = {
+            avatar_url: res.data.avatar_url,
+            html_url: res.data.html_url,
+            login: res.data.login,
+        };
 
-    if (res.data.name) html += `<div>Name: ${res.data.name}</div>`;
-    if (res.data.bio) html += `<div>Bio: ${res.data.bio}</div>`;
-    if (res.data.location) html += `<div>Location: ${res.data.location}</div>`;
-    if (res.data.public_repos)
-      html += `<div>Public Repos: ${res.data.public_repos}</div>`;
-    if (res.data.followers) {
-      if (res.data.followers === 0) html += `<div>`;
-      else html += `<div>Followers: ${res.data.followers}`;
-      if (res.data.following && res.data.following !== 0)
-        html += ` Following: ${res.data.following}</div>`;
-      else html += `</div>`;
+        if (res.data.name) obj = { ...obj, name: `Name: ${res.data.name}` };
+
+        if (res.data.bio) obj = { ...obj, bio: `Bio: ${res.data.bio}` };
+        if (res.data.location)
+            obj = { ...obj, location: `Location: ${res.data.location}` };
+        if (res.data.public_repos)
+            obj = {
+                ...obj,
+                public_repos: `Public Repos: ${res.data.public_repos}`,
+            };
+        if (res.data.followers) {
+            obj = { ...obj, followers: `Followers: ${res.data.followers}` };
+            if (res.data.following)
+                obj = { ...obj, following: `Following: ${res.data.following}` };
+        }
+
+        updateUserInfo(obj);
     }
 
-    return html;
-  }
+    function handleSearch(event) {
+        // updating login as user
+        // I tried "jfie", which exists
+        // then I tried "jfiee", which doesn't exist
+        // then I tried "jfie" again and this time it didn't work
+        // because login was still "jfie" and now user === login was true so no search was made
+        // so, here I change login to user each time something is searched
+        updateLogin(user);
 
-  function handleSearch(event) {
-    let userInfo = document.getElementById("user-info");
-    let errorText = document.getElementById("error-text");
+        console.log(`user = ${user} and login = ${login}`);
+        if (user === "" || user === null) {
+            console.log(`Empty user. user = ${user}`);
+            updateErrorText("Input field is empty");
 
-    // updating login as user
-    // I tried "jfie", which exists
-    // then I tried "jfiee", which doesn't exist
-    // then I tried "jfie" again and this time it didn't work
-    // because login was still "jfie" and now user === login was true so no search was made
-    // so, here I change login to user each time something is searched
-    updateLogin(user);
+            // removing the previous user results
+            updateUserInfo({ avatar_url: "", error: "" });
+        } else if (login !== user) {
+            // if login !== user only then a new API call is made
+            console.log("Clicked on search with user=" + user);
 
-    console.log(`user = ${user} and login = ${login}`);
-    if (user === "" || user === null) {
-      console.log(`Empty user. user = ${user}`);
-      errorText.innerHTML = `
-            <div style="color: red;">Input field is empty</div>
-            `;
-      // removing the previous user results
-      userInfo.innerHTML = "";
-    } else if (login !== user) {
-      // if login !== user only then a new API call is made
-      console.log("Clicked on search with user=" + user);
-
-      const regex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
-      if (!regex.test(user)) {
-        // adding error messages if the username pattern isn't satisfied
-        errorText.innerHTML = `
-            <div style="color: red;">Please enter a correct username</div>
-            `;
-        // removing the previous user results
-        userInfo.innerHTML = "";
-      } else {
-        // using axios for API calls
-        axios
-          .get(`https://api.github.com/users/${user}`)
-          .then((res) => {
-            console.log(res.data);
-            // updating the login state
-            updateLogin(res.data.login);
-            userInfo.innerHTML = addHTML(res);
-          })
-          .catch((error) => {
-            console.log(`${error.response.status} error is ${error}`);
-            // 404 means user not found for the particular username
-            if (error.response.status === 404) {
-              userInfo.innerHTML = `
-                            <div style="margin-top:20px;">Sorry, there seems to be no user with this username</div> 
-                            `;
+            const regex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
+            if (!regex.test(user)) {
+                // adding error messages if the username pattern isn't satisfied
+                updateErrorText("Please enter a correct username");
+                // removing the previous user results
+                updateUserInfo({});
+            } else {
+                // using axios for API calls
+                axios
+                    .get(`https://api.github.com/users/${user}`)
+                    .then((res) => {
+                        console.log(res.data);
+                        // updating the login state
+                        updateLogin(res.data.login);
+                        addHTML(res);
+                    })
+                    .catch((error) => {
+                        console.log(
+                            `${error.response.status} error is ${error}`
+                        );
+                        // 404 means user not found for the particular username
+                        if (error.response.status === 404) {
+                            updateUserInfo({
+                                error:
+                                    "Sorry, there seems to be no user with this username",
+                            });
+                        }
+                    });
             }
-          });
-      }
+        }
+        console.log(`userInfo = ${userInfo} ${userInfo.avatar_url}`);
+        event.preventDefault();
     }
-    event.preventDefault();
-  }
 
-  return (
-    <div style={{ fontSize: "20px" }}>
-      <form action="#" onSubmit={handleSearch}>
-        <input
-          id="text-field"
-          placeholder="Enter a username to search.."
-          value={user}
-          onChange={(ev) => {
-            document.getElementById("error-text").innerHTML = "";
-            changeUser(ev.target.value);
-          }}
-          type="text"
-          autoFocus
-        />
-        <button style={{ marginLeft: "20px" }}>Search</button>
-        {/*used to display error texts, if any*/}
-        <div id="error-text"></div>
-        {/*used to display user info, if available*/}
-        <div id="user-info"></div>
-      </form>
-    </div>
-  );
+    return (
+        <div style={{ fontSize: "20px" }}>
+            <form action="#" onSubmit={handleSearch}>
+                <input
+                    id="text-field"
+                    placeholder="Enter a username to search.."
+                    value={user}
+                    onChange={(ev) => {
+                        updateErrorText("");
+                        changeUser(ev.target.value);
+                    }}
+                    type="text"
+                    autoFocus
+                />
+                <button style={{ marginLeft: "20px" }}>Search</button>
+
+                {/*used to display error texts, if any*/}
+                <div style={{ color: "red" }}>{errorText}</div>
+
+                <div style={{ marginTop: "20px" }}>{userInfo.error}</div>
+
+                {/* conditional rendering, only if the avatar_url is available */}
+                {userInfo.avatar_url ? (
+                    <img
+                        style={{
+                            marignTop: "20px",
+                            borderRadius: "50%",
+                            width: "150px",
+                            height: "150px",
+                        }}
+                        alt="user avatar"
+                        src={userInfo.avatar_url}
+                    />
+                ) : null}
+                {userInfo.login ? (
+                    <div style={{ marginTop: "20px" }}>
+                        Username:{" "}
+                        <a href={userInfo.html_url}>{userInfo.login}</a>
+                    </div>
+                ) : null}
+                <div>{userInfo.name}</div>
+                <div>{userInfo.bio}</div>
+                <div>{userInfo.location}</div>
+                <div>{userInfo.public_repos}</div>
+                <div>
+                    {userInfo.followers} {userInfo.following}
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default Search;
